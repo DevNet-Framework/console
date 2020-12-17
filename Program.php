efca1542e81a525a50c116f70ac8757d16f7c550
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2020-12-17 03:36:38 -0500

    create console project

---
+++ Program.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application;
+
+use Artister\System\Console;
+
+class Program
+{
+    public static function main(array $args = [])
+    {
+        Console::writeline("Hello World!");
+    }
+}
